1d2e1ff3785ae6df4e33e619323c22383094b122
Peter Schilling <peter@peterschilling.org>
2012-12-14 02:27:28 -0500

    wait for that dom

--- app.coffee
+++ app.coffee
@@ -1,160 +1,162 @@
-dragging = false
-prev = { x: -100, y: -100 }
+$(document).ready ->
 
-sloth = new Image()
-imageLoaded = false
-
-# set up canvas
-canvas = document.getElementById('sloth-board')
-$canvas = $(canvas)
-offset = $canvas.offset()
-borderSize = parseInt($canvas.css("border-left-width"))
-canvas.width = $canvas.parent().width() - borderSize * 2
-canvas.height = $canvas.parent().height() - borderSize * 2
-ctx = canvas.getContext '2d'
-
-# set up bg canvas
-bgCanvas = document.getElementById 'bg-canvas'
-bgCanvas.width = canvas.width
-bgCanvas.height = canvas.height
-bgCtx = bgCanvas.getContext '2d'
-
-sloth.onload = ->
-  imageLoaded = true
-
-sloth.src = 'img/slothpal.png'
-
-drawSloth = (e) ->
-  return unless imageLoaded
-  x = e.pageX - offset.left - borderSize
-  y = e.pageY - offset.top - borderSize
-
-  threshhold =  16
-  if Math.abs(x - prev.x) < threshhold && Math.abs(y - prev.y) < threshhold
-    return
-
-  ctx.globalCompositeOperation = "source-over"
-  ctx.drawImage(sloth, x, y)
-
-  prev.x = x
-  prev.y = y
-
-erase = (e)->
-  x = e.pageX - offset.left - borderSize
-  y = e.pageY - offset.top - borderSize
-  radius = 20
-  ctx.globalCompositeOperation = "destination-out"
-  ctx.strokeStyle = "rgba(0,0,0,1)"
-  ctx.beginPath()
-  ctx.arc(x + radius, y + radius, radius, 0 , 2 * Math.PI, false)
-  ctx.closePath()
-  ctx.fill()
-
-reset = ->
-  ctx.clearRect(0,0, canvas.width, canvas.height)
-  bgCtx.clearRect(0, 0, canvas.width, canvas.height)
-
-$('#reset').on 'click', reset
-
-
-drawAction = drawSloth
-
-$(document).on 'mousedown', (e)->
-  dragging = true
-  drawAction(e)
-
-$(document).on 'mouseup mouseexit', (e)->
   dragging = false
+  prev = { x: -100, y: -100 }
 
-$(document).on 'mousemove', (e)->
-  if !dragging
-    return
+  sloth = new Image()
+  imageLoaded = false
 
-  drawAction(e)
+  # set up canvas
+  canvas = document.getElementById('sloth-board')
+  $canvas = $(canvas)
+  offset = $canvas.offset()
+  borderSize = parseInt($canvas.css("border-left-width"))
+  canvas.width = $canvas.parent().width() - borderSize * 2
+  canvas.height = $canvas.parent().height() - borderSize * 2
+  ctx = canvas.getContext '2d'
 
-slothMode = ->
-  $('#sloth-board').removeClass('erase')
+  # set up bg canvas
+  bgCanvas = document.getElementById 'bg-canvas'
+  bgCanvas.width = canvas.width
+  bgCanvas.height = canvas.height
+  bgCtx = bgCanvas.getContext '2d'
+
+  sloth.onload = ->
+    imageLoaded = true
+
+  sloth.src = 'img/slothpal.png'
+
+  drawSloth = (e) ->
+    return unless imageLoaded
+    x = e.pageX - offset.left - borderSize
+    y = e.pageY - offset.top - borderSize
+
+    threshhold =  16
+    if Math.abs(x - prev.x) < threshhold && Math.abs(y - prev.y) < threshhold
+      return
+
+    ctx.globalCompositeOperation = "source-over"
+    ctx.drawImage(sloth, x, y)
+
+    prev.x = x
+    prev.y = y
+
+  erase = (e)->
+    x = e.pageX - offset.left - borderSize
+    y = e.pageY - offset.top - borderSize
+    radius = 20
+    ctx.globalCompositeOperation = "destination-out"
+    ctx.strokeStyle = "rgba(0,0,0,1)"
+    ctx.beginPath()
+    ctx.arc(x + radius, y + radius, radius, 0 , 2 * Math.PI, false)
+    ctx.closePath()
+    ctx.fill()
+
+  reset = ->
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+    bgCtx.clearRect(0, 0, canvas.width, canvas.height)
+
+  $('#reset').on 'click', reset
+
+
   drawAction = drawSloth
 
-eraseMode = ->
-  $('#sloth-board').addClass('erase')
-  drawAction = erase
+  $(document).on 'mousedown', (e)->
+    dragging = true
+    drawAction(e)
 
-$('#eraser').on 'click', eraseMode
-$('#sloth').on 'click', slothMode
+  $(document).on 'mouseup mouseexit', (e)->
+    dragging = false
 
-# http://stackoverflow.com/questions/6388284
-$(document).on 'selectstart dragstart', (e)->
-  e.preventDefault()
+  $(document).on 'mousemove', (e)->
+    if !dragging
+      return
 
-## http://stackoverflow.com/a/2931668/692224
-document.body.style.MozUserSelect="none"
+    drawAction(e)
 
-#$('.js-save').on 'click', ->
-  #coords = []
-  #$('.sloth').each ->
-    #pos = $(this).position()
-    #coords.push
-      #x: pos.left
-      #y: pos.top
+  slothMode = ->
+    $('#sloth-board').removeClass('erase')
+    drawAction = drawSloth
 
-  #debugger
+  eraseMode = ->
+    $('#sloth-board').addClass('erase')
+    drawAction = erase
 
-$('#save').on 'click', ->
-  output = document.createElement 'canvas'
-  output.width = canvas.width
-  output.height = canvas.height
-  oCtx = output.getContext '2d'
-  oCtx.drawImage bgCanvas, 0, 0
-  oCtx.drawImage canvas, 0, 0
-  mime = "image/png"
-  data = output.toDataURL mime
-  window.open(data, 'sloth.png')
+  $('#eraser').on 'click', eraseMode
+  $('#sloth').on 'click', slothMode
 
-## http://stackoverflow.com/a/11583627/692224
-handleFileSelect = (evt) ->
-  files = evt.target.files # FileList object
+  # http://stackoverflow.com/questions/6388284
+  $(document).on 'selectstart dragstart', (e)->
+    e.preventDefault()
 
-  # Loop through the FileList and render image files as thumbnails.
-  i = 0
-  f = undefined
+  ## http://stackoverflow.com/a/2931668/692224
+  document.body.style.MozUserSelect="none"
 
-  while f = files[i]
+  #$('.js-save').on 'click', ->
+    #coords = []
+    #$('.sloth').each ->
+      #pos = $(this).position()
+      #coords.push
+        #x: pos.left
+        #y: pos.top
 
-    # Only process image files.
-    continue  unless f.type.match("image.*")
-    reader = new FileReader()
+    #debugger
 
-    # Closure to capture the file information.
-    reader.onload = ((theFile) ->
-      (e) ->
-        img = new Image()
-        img.onload = ->
+  $('#save').on 'click', ->
+    output = document.createElement 'canvas'
+    output.width = canvas.width
+    output.height = canvas.height
+    oCtx = output.getContext '2d'
+    oCtx.drawImage bgCanvas, 0, 0
+    oCtx.drawImage canvas, 0, 0
+    mime = "image/png"
+    data = output.toDataURL mime
+    window.open(data, 'sloth.png')
+
+  ## http://stackoverflow.com/a/11583627/692224
+  handleFileSelect = (evt) ->
+    files = evt.target.files # FileList object
+
+    # Loop through the FileList and render image files as thumbnails.
+    i = 0
+    f = undefined
+
+    while f = files[i]
+
+      # Only process image files.
+      continue  unless f.type.match("image.*")
+      reader = new FileReader()
+
+      # Closure to capture the file information.
+      reader.onload = ((theFile) ->
+        (e) ->
+          img = new Image()
+          img.onload = ->
 
 
-          w = img.width
-          h = img.height
-          r = w / h
+            w = img.width
+            h = img.height
+            r = w / h
 
-          if img.width > canvas.width
-            w = canvas.width
-            h = w / r
+            if img.width > canvas.width
+              w = canvas.width
+              h = w / r
 
-          if img.height > canvas.height
-            h = canvas.height
-            w = h * r
+            if img.height > canvas.height
+              h = canvas.height
+              w = h * r
 
-          gapX = canvas.width - w
-          gapY = canvas.height - h
+            gapX = canvas.width - w
+            gapY = canvas.height - h
 
-          reset()
-          bgCtx.drawImage(img, gapX / 2, gapY / 2, w, h)
+            reset()
+            bgCtx.drawImage(img, gapX / 2, gapY / 2, w, h)
 
-        img.src = e.target.result
-    )(f)
+          img.src = e.target.result
+      )(f)
 
-    reader.readAsDataURL f
-    i++
+      reader.readAsDataURL f
+      i++
 
-$(document).on 'change', handleFileSelect
+  $(document).on 'change', handleFileSelect
 
--- app.js
+++ app.js
@@ -1,169 +1,138 @@
 (function() {
-  var $canvas, bgCanvas, bgCtx, borderSize, canvas, ctx, dragging, drawAction, drawSloth, erase, eraseMode, handleFileSelect, imageLoaded, offset, prev, reset, sloth, slothMode;
 
-  dragging = false;
-
-  prev = {
-    x: -100,
-    y: -100
-  };
-
-  sloth = new Image();
-
-  imageLoaded = false;
-
-  canvas = document.getElementById('sloth-board');
-
-  $canvas = $(canvas);
-
-  offset = $canvas.offset();
-
-  borderSize = parseInt($canvas.css("border-left-width"));
-
-  canvas.width = $canvas.parent().width() - borderSize * 2;
-
-  canvas.height = $canvas.parent().height() - borderSize * 2;
-
-  ctx = canvas.getContext('2d');
-
-  bgCanvas = document.getElementById('bg-canvas');
-
-  bgCanvas.width = canvas.width;
-
-  bgCanvas.height = canvas.height;
-
-  bgCtx = bgCanvas.getContext('2d');
-
-  sloth.onload = function() {
-    return imageLoaded = true;
-  };
-
-  sloth.src = 'img/slothpal.png';
-
-  drawSloth = function(e) {
-    var threshhold, x, y;
-    if (!imageLoaded) return;
-    x = e.pageX - offset.left - borderSize;
-    y = e.pageY - offset.top - borderSize;
-    threshhold = 16;
-    if (Math.abs(x - prev.x) < threshhold && Math.abs(y - prev.y) < threshhold) {
-      return;
-    }
-    ctx.globalCompositeOperation = "source-over";
-    ctx.drawImage(sloth, x, y);
-    prev.x = x;
-    return prev.y = y;
-  };
-
-  erase = function(e) {
-    var radius, x, y;
-    x = e.pageX - offset.left - borderSize;
-    y = e.pageY - offset.top - borderSize;
-    radius = 20;
-    ctx.globalCompositeOperation = "destination-out";
-    ctx.strokeStyle = "rgba(0,0,0,1)";
-    ctx.beginPath();
-    ctx.arc(x + radius, y + radius, radius, 0, 2 * Math.PI, false);
-    ctx.closePath();
-    return ctx.fill();
-  };
-
-  reset = function() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    return bgCtx.clearRect(0, 0, canvas.width, canvas.height);
-  };
-
-  $('#reset').on('click', reset);
-
-  drawAction = drawSloth;
-
-  $(document).on('mousedown', function(e) {
-    dragging = true;
-    return drawAction(e);
-  });
-
-  $(document).on('mouseup mouseexit', function(e) {
-    return dragging = false;
-  });
-
-  $(document).on('mousemove', function(e) {
-    if (!dragging) return;
-    return drawAction(e);
-  });
-
-  slothMode = function() {
-    $('#sloth-board').removeClass('erase');
-    return drawAction = drawSloth;
-  };
-
-  eraseMode = function() {
-    $('#sloth-board').addClass('erase');
-    return drawAction = erase;
-  };
-
-  $('#eraser').on('click', eraseMode);
-
-  $('#sloth').on('click', slothMode);
-
-  $(document).on('selectstart dragstart', function(e) {
-    return e.preventDefault();
-  });
-
-  document.body.style.MozUserSelect = "none";
-
-  $('#save').on('click', function() {
-    var data, mime, oCtx, output;
-    output = document.createElement('canvas');
-    output.width = canvas.width;
-    output.height = canvas.height;
-    oCtx = output.getContext('2d');
-    oCtx.drawImage(bgCanvas, 0, 0);
-    oCtx.drawImage(canvas, 0, 0);
-    mime = "image/png";
-    data = output.toDataURL(mime);
-    return window.open(data, 'sloth.png');
-  });
-
-  handleFileSelect = function(evt) {
-    var f, files, i, reader, _results;
-    files = evt.target.files;
-    i = 0;
-    f = void 0;
-    _results = [];
-    while (f = files[i]) {
-      if (!f.type.match("image.*")) continue;
-      reader = new FileReader();
-      reader.onload = (function(theFile) {
-        return function(e) {
-          var img;
-          img = new Image();
-          img.onload = function() {
-            var gapX, gapY, h, r, w;
-            w = img.width;
-            h = img.height;
-            r = w / h;
-            if (img.width > canvas.width) {
-              w = canvas.width;
-              h = w / r;
-            }
-            if (img.height > canvas.height) {
-              h = canvas.height;
-              w = h * r;
-            }
-            gapX = canvas.width - w;
-            gapY = canvas.height - h;
-            reset();
-            return bgCtx.drawImage(img, gapX / 2, gapY / 2, w, h);
+  $(document).ready(function() {
+    var $canvas, bgCanvas, bgCtx, borderSize, canvas, ctx, dragging, drawAction, drawSloth, erase, eraseMode, handleFileSelect, imageLoaded, offset, prev, reset, sloth, slothMode;
+    dragging = false;
+    prev = {
+      x: -100,
+      y: -100
+    };
+    sloth = new Image();
+    imageLoaded = false;
+    canvas = document.getElementById('sloth-board');
+    $canvas = $(canvas);
+    offset = $canvas.offset();
+    borderSize = parseInt($canvas.css("border-left-width"));
+    canvas.width = $canvas.parent().width() - borderSize * 2;
+    canvas.height = $canvas.parent().height() - borderSize * 2;
+    ctx = canvas.getContext('2d');
+    bgCanvas = document.getElementById('bg-canvas');
+    bgCanvas.width = canvas.width;
+    bgCanvas.height = canvas.height;
+    bgCtx = bgCanvas.getContext('2d');
+    sloth.onload = function() {
+      return imageLoaded = true;
+    };
+    sloth.src = 'img/slothpal.png';
+    drawSloth = function(e) {
+      var threshhold, x, y;
+      if (!imageLoaded) return;
+      x = e.pageX - offset.left - borderSize;
+      y = e.pageY - offset.top - borderSize;
+      threshhold = 16;
+      if (Math.abs(x - prev.x) < threshhold && Math.abs(y - prev.y) < threshhold) {
+        return;
+      }
+      ctx.globalCompositeOperation = "source-over";
+      ctx.drawImage(sloth, x, y);
+      prev.x = x;
+      return prev.y = y;
+    };
+    erase = function(e) {
+      var radius, x, y;
+      x = e.pageX - offset.left - borderSize;
+      y = e.pageY - offset.top - borderSize;
+      radius = 20;
+      ctx.globalCompositeOperation = "destination-out";
+      ctx.strokeStyle = "rgba(0,0,0,1)";
+      ctx.beginPath();
+      ctx.arc(x + radius, y + radius, radius, 0, 2 * Math.PI, false);
+      ctx.closePath();
+      return ctx.fill();
+    };
+    reset = function() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      return bgCtx.clearRect(0, 0, canvas.width, canvas.height);
+    };
+    $('#reset').on('click', reset);
+    drawAction = drawSloth;
+    $(document).on('mousedown', function(e) {
+      dragging = true;
+      return drawAction(e);
+    });
+    $(document).on('mouseup mouseexit', function(e) {
+      return dragging = false;
+    });
+    $(document).on('mousemove', function(e) {
+      if (!dragging) return;
+      return drawAction(e);
+    });
+    slothMode = function() {
+      $('#sloth-board').removeClass('erase');
+      return drawAction = drawSloth;
+    };
+    eraseMode = function() {
+      $('#sloth-board').addClass('erase');
+      return drawAction = erase;
+    };
+    $('#eraser').on('click', eraseMode);
+    $('#sloth').on('click', slothMode);
+    $(document).on('selectstart dragstart', function(e) {
+      return e.preventDefault();
+    });
+    document.body.style.MozUserSelect = "none";
+    $('#save').on('click', function() {
+      var data, mime, oCtx, output;
+      output = document.createElement('canvas');
+      output.width = canvas.width;
+      output.height = canvas.height;
+      oCtx = output.getContext('2d');
+      oCtx.drawImage(bgCanvas, 0, 0);
+      oCtx.drawImage(canvas, 0, 0);
+      mime = "image/png";
+      data = output.toDataURL(mime);
+      return window.open(data, 'sloth.png');
+    });
+    handleFileSelect = function(evt) {
+      var f, files, i, reader, _results;
+      files = evt.target.files;
+      i = 0;
+      f = void 0;
+      _results = [];
+      while (f = files[i]) {
+        if (!f.type.match("image.*")) continue;
+        reader = new FileReader();
+        reader.onload = (function(theFile) {
+          return function(e) {
+            var img;
+            img = new Image();
+            img.onload = function() {
+              var gapX, gapY, h, r, w;
+              w = img.width;
+              h = img.height;
+              r = w / h;
+              if (img.width > canvas.width) {
+                w = canvas.width;
+                h = w / r;
+              }
+              if (img.height > canvas.height) {
+                h = canvas.height;
+                w = h * r;
+              }
+              gapX = canvas.width - w;
+              gapY = canvas.height - h;
+              reset();
+              return bgCtx.drawImage(img, gapX / 2, gapY / 2, w, h);
+            };
+            return img.src = e.target.result;
           };
-          return img.src = e.target.result;
-        };
-      })(f);
-      reader.readAsDataURL(f);
-      _results.push(i++);
-    }
-    return _results;
-  };
-
-  $(document).on('change', handleFileSelect);
+        })(f);
+        reader.readAsDataURL(f);
+        _results.push(i++);
+      }
+      return _results;
+    };
+    return $(document).on('change', handleFileSelect);
+  });
 
 }).call(this);
