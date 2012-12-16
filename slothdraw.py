2484dcec70972f63583f09ef4fd69f5d43f7dbff
Peter Schilling <peter@peterschilling.org>
2012-12-16 15:55:11 -0500

    caching for mad speedz

--- slothdraw.py
+++ slothdraw.py
@@ -1,9 +1,10 @@
 import webapp2
 import jinja2
 import os, urllib2, re, base64, urllib
+from time import gmtime, strftime
 
 from google.appengine.ext import db
-from google.appengine.api import images, files
+from google.appengine.api import images, files, memcache
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -43,41 +44,81 @@ class Draw(webapp2.RequestHandler):
 
 class FetchSloth(webapp2.RequestHandler):
     def get(self, sloth_path):
-        sloth = SlothDrawing.get_by_key_name(sloth_path)
+        memcached_key = 'sloth:%s' % sloth_path
+        sloth_image = memcache.get(memcached_key)
 
-        if sloth == None:
-            self.response.set_status(404)
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write('booourns no sloths here check your url!')
-            return
+        if sloth_image is not None:
+            self.response.headers['Content-Type'] = 'image/png'
+            self.response.out.write(sloth_image)
+        else:
 
-        self.response.headers['Content-Type'] = 'image/png'
-        self.response.out.write(sloth.image)
+            sloth = SlothDrawing.get_by_key_name(sloth_path)
+
+            if sloth == None:
+                self.response.set_status(404)
+                self.response.headers['Content-Type'] = 'text/plain'
+                self.response.out.write('booourns no sloths here check your url!')
+                return
+
+            sloth_image = sloth.image
+
+            if not memcache.add(memcached_key, sloth_image, 3600 * 24 * 5):
+                logging.error('Memcache set failed.')
+
+            self.response.headers['Content-Type'] = 'image/png'
+            self.response.out.write(sloth_image)
 
 class Fetch(webapp2.RequestHandler):
     def get(self, sloth_path):
+        memcached_key = 'fetch:%s' % sloth_path
 
-        # smrt ppl wouldn't hard code this
-        # i'm not claiming to be one of them
-        share_url_enc = urllib.quote_plus('http://sloths.arerad.com/' +
-            sloth_path)
+        fetched = memcache.get(memcached_key)
 
-        template = jinja_environment.get_template('fetch.html')
-        data = {
-            'sloth_path': sloth_path,
+        if fetched is not None:
+            self.response.out.write(fetched)
+        else:
 
-            'twitter_share_url':
-                'https://twitter.com/share',
+            # smrt ppl wouldn't hard code this
+            # i'm not claiming to be one of them
+            share_url_enc = urllib.quote_plus('http://sloths.arerad.com/' +
+                sloth_path)
 
-            'facebook_share_url':
-                'https://www.facebook.com/sharer/sharer.php?u=' + share_url_enc
-        }
-        self.response.out.write(template.render(data))
+            template = jinja_environment.get_template('fetch.html')
+            data = {
+                'sloth_path': sloth_path,
+
+                'twitter_share_url':
+                    'https://twitter.com/share',
+
+                'facebook_share_url':
+                    'https://www.facebook.com/sharer/sharer.php?u=' +
+                    share_url_enc,
+
+                'render_date':
+                    strftime("%Y-%m-%d %H:%M:%S", gmtime())
+            }
+
+            fetched = template.render(data)
+
+            if not memcache.add(memcached_key, fetched, 3600 * 24 * 5):
+                logging.error('Memcache set failed.')
+
+            self.response.out.write(fetched)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('index.html')
-        self.response.out.write(template.render({}))
+        homepage = memcache.get('homepage')
+        if homepage is not None:
+            self.response.out.write(homepage)
+        else:
+            template = jinja_environment.get_template('index.html')
+            homepage = template.render({ 'render_date':
+                    strftime("%Y-%m-%d %H:%M:%S", gmtime()) })
+
+            if not memcache.add('homepage', homepage, 3600 * 24 * 5):
+                logging.error('Memcache set failed.')
+
+            self.response.out.write(homepage)
 
 app = webapp2.WSGIApplication([(r'/', MainPage),
                                (r'/draw', Draw),
